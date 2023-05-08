c865cd9fcd517207dba230c2a95b2604fe929b25
Apdo-25 <abdo0062@easv365.dk>
2023-05-08 16:42:43 +0200

    changes to project model

--- Frontend/src/components/UserAvatar.vue
+++ Frontend/src/components/UserAvatar.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 import { useAuthStore } from '@/stores/auth'
-
-const props = defineProps({
+defineProps({
   username: {
     type: String,
     required: true
@@ -10,10 +9,6 @@ const props = defineProps({
   avatar: {
     type: String,
     default: null
-  },
-  api: {
-    type: String,
-    default: 'avataaars'
   }
 })
 
