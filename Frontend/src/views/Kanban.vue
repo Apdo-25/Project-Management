978516093db4fc99e462fe2312e24a0e865d556d
Apdo-25 <abdo0062@easv365.dk>
2023-05-12 17:12:06 +0200

    test

--- Frontend/src/views/Kanban.vue
+++ Frontend/src/views/Kanban.vue
@@ -8,3 +8,41 @@ import Layout from '@/layouts/Layout.vue'
     <SectionMain> This is the kanban board page</SectionMain>
   </Layout>
 </template>
+=======
+<!-- src/views/Kanban.vue -->
+<template>
+  <Layout>
+    <LayoutS>
+      <SectionMain>
+        <div class="p-4">
+          <h1 class="text-3xl font-bold mb-4">Kanban Board</h1>
+          <Board :columns="columns" />
+        </div>
+      </SectionMain>
+    </LayoutS>
+  </Layout>
+
+  <script>
+    import { computed } from 'vue'
+    import SectionMain from '@/components/SectionMain.vue'
+    import { useKanbanStore } from '@/stores/Kanban'
+    import Layout from '@/layouts/Layout.vue'
+    import Board from '@/components/Board.vue'
+
+    export default {
+      components: {
+        SectionMain,
+        Layout,
+        Board
+      },
+      setup() {
+        const kanbanStore = useKanbanStore()
+        const columns = computed(() => kanbanStore.columns)
+
+        return {
+          columns
+        }
+      }
+    }
+  </script>
+</template>
